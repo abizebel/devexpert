da63c28f459ba454d88d522dd694e0045a0c14f2
Abbas <abizebel@gmail.com>
2019-09-15 21:05:01 +0430

    Complete start page

--- src/containers/startContainer.tsx
+++ src/containers/startContainer.tsx
@@ -45,21 +45,21 @@ class StartContainer extends Component <Props, State> {
           <Text style={styles.description}>یاد گیری مباحث تخصصی و مشارکت در پرسش و پاسخ و یافتن شغل </Text>
         </View>
         <View style={styles.buttonContainer}>
-        <Button
-          onPress={()=>{
-            Alert.alert('ohhh')
-          }}
-          title="شروع یادگیری"
-          buttonStyle={styles.startButton}
-          textStyle={{fontFamily : "IRANSansMobile(FaNum)_Medium"}}
-          ViewComponent={LinearGradient} // Don't forget this!
-          linearGradientProps={{
-            colors: ['#FFC107', '#B78C09'],
-            start: { x: 0, y: 0.5 },
-            end: { x: 1, y: 0.5 },
-          }}
-        />
 
+          <Button 
+            onPress={()=>{
+              Alert.alert('ohhh')
+            }}
+            ViewComponent={LinearGradient} // Don't forget this!
+            linearGradientProps={{
+              colors: ['#FFC107', '#B78C09'],
+              start: { x: 0, y: 0.5 },
+              end: { x: 1, y: 0.5 },
+            }}
+            buttonStyle={styles.startButton}
+            title="شروع یادگیری" 
+            titleStyle={{ fontFamily:'IRANSansMobile(FaNum)_Medium' }}
+           />
 
         </View>
         
@@ -89,7 +89,7 @@ const styles = StyleSheet.create({
     fontFamily : 'IRANSansMobile(FaNum)_Medium',
     fontSize :36,
     marginBottom: 120,
-    marginTop: 60
+    marginTop: 60,
   },
   subtitle : {
     textAlign:'center',
@@ -107,12 +107,13 @@ const styles = StyleSheet.create({
   buttonContainer : {
     flex:1, 
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
   },
   startButton : {
     padding:12,
     width:300,
     borderRadius:50,
+  
 
   }
 
