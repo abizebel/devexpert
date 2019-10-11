fb53764acebdd619a6edfadb99678435ee6fdd5a
Abbas <abizebel@gmail.com>
2019-10-11 22:56:03 +0330

    single post

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 import React, {Fragment, Component} from 'react';
 import HomeContainer from './src/containers/homeContainer';
 import WelcomeContainer from './src/containers/welcomeContainer';
-
+import SingleContainer from './src/containers/singleContainer';
 import StartContainer from './src/containers/startContainer';
 import { NativeRouter, Route, Link } from "react-router-native";
 
@@ -19,7 +19,7 @@ class App extends Component <{}, {}> {
 
 
         <Route exact path="/home" component={StartContainer} />
-        <Route exact path="/" component={HomeContainer} />
+        <Route exact path="/" component={SingleContainer} />
       </NativeRouter>
 
     )
